653e5d99ab89d9c63e303ef90acfb853fd5bc0d8
Asunuwar <sunuwar.arun@gmail.com>
2020-01-21 21:04:48 -0500

    edit css

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $('button').click(function () {
-        $('#todo').append("<ul>" + $("input[name=task]").val() + " <a href='#' class='close' aria-hidden='true'>&times;</a></ul>");
+        $('#todo').append("<ul>" + $("input[name=task]").val() + "<a href='#' class='close' aria-hidden='true'>&times;</a></ul>");
     });
     $("body").on('click', '#todo a', function () {
         $(this).closest("ul").remove();
